11148bbe6a793b4067addc040b9035603579e3ec
scott <slfaludi@gmail.com>
2024-03-04 13:34:08 -0800

    added media builder sample

---
+++ media_builder/Readme.md
@@ -0,0 +1,152 @@
+
+## Prerequisites
+
+- Pip
+
+> 📘 Note
+> 
+> You can download pip [here](https://pip.pypa.io/en/stable/installation/).
+
+## Nomad SDK PIP
+
+To learn how to download and setup the nomad sdk pip, go to [Nomad SDK PIP](https://github.com/Nomad-Media/nomad-sdk/tree/main/nomad-sdk-pip).
+
+## Create Media Builder
+
+To create a media builder, enter 1 when propmted. Then enter the name you want to give to your media builder. Optionally, you can also enter the id of the destination folder where you want the media builder to be created and the collections, related contents, and properties you want to add to the media builder.
+
+![](images/create-media-builder.png)
+
+> 📘 Note
+>
+> For more information on the API used, go to [Create Media Builder](https://developer.nomad-cms.com/reference/post_mediabuilder).
+
+## Create Media Builder Item
+
+To create a media builder item, enter 2 when propmted. Then enter the id media builder and source asset, and optionally the start or the start and end time code, or the id of the annotation you want to add to the media builder item. Optionally you can also add related contents.
+
+![](images/create-media-builder-item.png)
+
+> 📘 Note
+>
+> For more information about the API call used, go to [Create Media Builder Item](https://developer.nomad-cms.com/reference/post_mediabuilder-mediabuilderid-items).
+
+## Create Media Builder Items Add Annotations
+
+To create media builder items from an asset using its annotations, enter 3 when propmted. Then enter the id of the media builder and source asset.
+
+![](images/create-media-builder-items-add-annotations.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Create Media Builder Item Add Annotations](https://developer.nomad-cms.com/reference/post_mediabuilder-mediabuilderid-items-sourceassetid-add-annotations).
+
+## Create Media Builder Items Bulk
+
+To create media builder items in bulk, enter 4 when propmted. Then enter the id of the media builder. Then add the source asset id, start time code, and optionally the end time code of the first media builder item you want to create. Then, if you want to add more media builder items, enter y when prompted and add the information for the next media builder item until you are done, then enter n when promted if you want to add another media builder.
+
+![](images/create-media-builder-items-bulk.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Create Media Builder Items Bulk](https://developer.nomad-cms.com/reference/post_mediabuilder-mediabuilderid-items-bulk).
+
+## Delete Media Builder
+
+To delete a media builder, enter 5 when propmted. Then enter the id of the media builder you want to delete.
+
+![](images/delete-media-builder.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Delete Media Builder](https://developer.nomad-cms.com/reference/delete_mediabuilder-mediabuilderid).
+
+## Delete Media Builder Item
+
+To delete a media builder item, enter 6 when propmted. Then enter the id of the media builder and the id of the media builder item you want to delete.
+
+![](images/delete-media-builder-item.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Delete Media Builder Item](https://developer.nomad-cms.com/reference/delete_mediabuilder-mediabuilderid-items-itemid).
+
+## Duplicate Media Builder
+
+To duplicate a media builder, enter 7 when propmted. Then enter the id of the media builder you want to duplicate and the name you want to give to the duplicated media builder. Optionally, you can also enter the id of the destination folder where you want the duplicated media builder to be created and the collections, related contents, and properties you want to add to the duplicated media builder.
+
+![](images/duplicate-media-builder.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Duplicate Media Builder](https://developer.nomad-cms.com/reference/post_mediabuilder-mediabuilderid-duplicate).
+
+## Get Media Builder
+
+To get a media builder, enter 8 when propmted. Then enter the id of the media builder you want to get.
+
+![](images/get-media-builder.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Get Media Builder](https://developer.nomad-cms.com/reference/get_mediabuilder-mediabuilderid).
+
+## Get Media Builders
+
+To get all media builders, enter 9 when propmted.
+
+![](images/get-media-builders.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Get Media Builders](https://developer.nomad-cms.com/reference/get_mediabuilder).
+
+## Get Media Builder Ids from an asset
+
+To get the media builder ids from an asset, enter 14 when propmted. Then enter the id of the asset you want to get the media builder ids from.
+
+![](images/get-media-builder-ids-from-asset.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Get Media Builder Ids from an asset](https://developer.nomad-cms.com/reference/get_mediabuilder-idsbysource-sourceassetid).
+
+## Get Media Builder Items
+
+To get all media builder items from a media builder, enter 10 when propmted. Then enter the id of the media builder you want to get the media builder items from.
+
+![](images/get-media-builder-items.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Get Media Builder Items](https://developer.nomad-cms.com/reference/get_mediabuilder-mediabuilderid-items).
+
+## Move Media Builder Items
+
+To move media builder items from one media builder to another, enter 11 when propmted. Then enter the id of the media builder the media builder is in and the media builder is in. If you don't want to move the media builder to the top of the list, enter the id of the media builder item you want to move the media builder items after.
+
+![](images/move-media-builder-items.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Move Media Builder Items](https://developer.nomad-cms.com/reference/post_mediabuilder-mediabuilderid-items-itemid-move).
+
+## Render Media Builder
+
+To render a media builder, enter 12 when propmted. Then enter the id of the media builder you want to render.
+
+![](images/render-media-builder.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Render Media Builder](https://developer.nomad-cms.com/reference/post_mediabuilder-mediabuilderid-render).
+
+## Update Media Builder
+
+To update a media builder, enter 13 when propmted. Then enter the id of the media builder you want to update. Optionally, you can also enter the name you want to give to the media builder, the id of the destination folder where you want the media builder to be created, and the collections, related contents, and properties you want to add to the media builder.
+
+![](images/update-media-builder.png)
+
+> 📘 Note
+>
+> For more information on the API call used, go to [Update Media Builder](https://developer.nomad-cms.com/reference/put_mediabuilder-mediabuilderid).
---
+++ media_builder/media_builder.py
@@ -0,0 +1,218 @@
+import sys, os
+sys.path.append(os.path.realpath('./nomad-samples'))
+
+from nomad_media_pip.nomad_sdk import Nomad_SDK
+from config import config
+
+nomad_sdk = Nomad_SDK(config)
+
+import json
+
+def get_input(prompt, required):
+    return input(f"Enter {prompt}: ") if required or input(f"Do you want to add {prompt} (y/n): ") == "y" else None
+
+def get_bool(prompt):
+    return True if input(f"{prompt} (y/n): ") == "y" else False
+
+def create_media_builder():
+    try:
+        NAME = get_input("name", True)
+        DESTINATION_FOLDER_ID = get_input("destination folder id", False)
+        COLLECTIONS = get_input("collections", False)
+        RELATED_CONTENTS = get_input("related contents", False)
+        PROPERTIES = get_input("properties", False)
+
+        MEDIA_BUILD = nomad_sdk.create_media_builder(NAME, DESTINATION_FOLDER_ID, COLLECTIONS,
+            RELATED_CONTENTS, PROPERTIES)
+        
+        print(json.dumps(MEDIA_BUILD, indent=4))
+    except:
+        raise Exception()
+    
+def create_media_builder_item():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+        SOURCE_ASSET_ID = get_input("source asset id", False)
+        START_TIME_CODE = get_input("start time code", False)
+        END_TIME_CODE = get_input("end time code", False)
+        SOURCE_ANNOTATION_ID = get_input("source annotation id", False)
+        RELATED_CONTENTS = get_input("related contents", False)
+
+        MEDIA_BUILD_ITEM = nomad_sdk.create_media_builder_item(MEDIA_BUILDER_ID, SOURCE_ASSET_ID,
+            START_TIME_CODE, END_TIME_CODE, SOURCE_ANNOTATION_ID, RELATED_CONTENTS)
+        
+        print(json.dumps(MEDIA_BUILD_ITEM, indent=4))
+    except:
+        raise Exception()
+    
+def create_media_builder_items_add_annotations():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+        SOURCE_ASSET_ID = get_input("source asset id", True)
+
+        MEDIA_BUILD_ITEMS = nomad_sdk.create_media_builder_items_add_annotations(MEDIA_BUILDER_ID, SOURCE_ASSET_ID)
+
+        print(json.dumps(MEDIA_BUILD_ITEMS, indent=4))
+    except:
+        raise Exception()
+
+def create_media_builder_items_bulk():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+        MEDIA_BUIDER_ITEMS = []
+
+        while True:
+            MEDIA_BUILDER_ITEM = {
+                "sourceAssetId": get_input("source asset id", True),
+                "startTimeCode": get_input("start time code", True),
+                "endTimeCode": get_input("end time code", False)
+            }
+
+            MEDIA_BUIDER_ITEMS.append(MEDIA_BUILDER_ITEM)
+
+            if input("Do you want to add another media builder item (y/n): ") == "n":
+                break
+
+        MEDIA_BUILD_ITEMS = nomad_sdk.create_media_builder_items_bulk(MEDIA_BUILDER_ID, MEDIA_BUIDER_ITEMS)
+
+        print(json.dumps(MEDIA_BUILD_ITEMS, indent=4))
+    except:
+        raise Exception()
+    
+def delete_media_builder():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+
+        nomad_sdk.delete_media_builder(MEDIA_BUILDER_ID)
+    except:
+        raise Exception()
+    
+def delete_media_builder_item():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+        MEDIA_BUILDER_ITEM_ID = get_input("media builder item id", True)
+
+        nomad_sdk.delete_media_builder_item(MEDIA_BUILDER_ID, MEDIA_BUILDER_ITEM_ID)
+    except:
+        raise Exception()
+    
+def duplicate_media_builder():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+        NAME = get_input("name", True)
+        DESTINATION_FOLDER_ID = get_input("destination folder id", False)
+        COLLECTIONS = get_input("collections", False)
+        RELATED_CONTENTS = get_input("related contents", False)
+        PROPERTIES = get_input("properties", False)
+
+        MEDIA_BUILD = nomad_sdk.duplicate_media_builder(MEDIA_BUILDER_ID, NAME, DESTINATION_FOLDER_ID,
+            COLLECTIONS, RELATED_CONTENTS, PROPERTIES)
+        
+        print(json.dumps(MEDIA_BUILD, indent=4))
+    except:
+        raise Exception()
+    
+def get_media_builder():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+
+        MEDIA_BUILD = nomad_sdk.get_media_builder(MEDIA_BUILDER_ID)
+        
+        print(json.dumps(MEDIA_BUILD, indent=4))
+    except:
+        raise Exception()
+    
+def get_media_builders():
+    try:
+        MEDIA_BUILDS = nomad_sdk.get_media_builders()
+        
+        print(json.dumps(MEDIA_BUILDS, indent=4))
+    except:
+        raise Exception()
+    
+def get_media_builder_items():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+
+        MEDIA_BUILD_ITEMS = nomad_sdk.get_media_builder_items(MEDIA_BUILDER_ID)
+        
+        print(json.dumps(MEDIA_BUILD_ITEMS, indent=4))
+    except:
+        raise Exception()
+    
+def move_media_builder_item():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+        MEDIA_BUILDER_ITEM_ID = get_input("media builder item id", True)
+        PREVIOUS_MEDIA_BUILDER_ITEM_ID = get_input("previous media builder item id", False)
+
+        nomad_sdk.move_media_builder_item(MEDIA_BUILDER_ID, MEDIA_BUILDER_ITEM_ID,
+            PREVIOUS_MEDIA_BUILDER_ITEM_ID)
+    except:
+        raise Exception()
+    
+def render_media_builder():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+        
+        nomad_sdk.render_media_builder(MEDIA_BUILDER_ID)
+    except:
+        raise Exception()
+    
+def update_media_builder():
+    try:
+        MEDIA_BUILDER_ID = get_input("media builder id", True)
+        NAME = get_input("name", False)
+        DESTINATION_FOLDER_ID = get_input("destination folder id", False)
+        COLLECTIONS = get_input("collections", False)
+        RELATED_CONTENTS = get_input("related contents", False)
+        PROPERTIES = get_input("properties", False)
+
+        MEDIA_BUILD = nomad_sdk.update_media_builder(MEDIA_BUILDER_ID, NAME, DESTINATION_FOLDER_ID,
+            COLLECTIONS, RELATED_CONTENTS, PROPERTIES)
+        
+        print(json.dumps(MEDIA_BUILD, indent=4))
+    except:
+        raise Exception()
+    
+def get_media_builder_ids_from_asset():
+    try:
+        SOURCE_ASSET_ID = get_input("source asset id", True)
+
+        MEDIA_BUILDER_IDS = nomad_sdk.get_media_builder_ids_from_asset(SOURCE_ASSET_ID)
+
+        print(json.dumps(MEDIA_BUILDER_IDS, indent=4))
+    except:
+        raise Exception()
+
+    
+FUNCTIONS = {
+    "1": create_media_builder,
+    "2": create_media_builder_item,
+    "3": create_media_builder_items_add_annotations,
+    "4": create_media_builder_items_bulk,
+    "5": delete_media_builder,
+    "6": delete_media_builder_item,
+    "7": duplicate_media_builder,
+    "8": get_media_builder,
+    "9": get_media_builders,
+    "10": get_media_builder_items,
+    "11": move_media_builder_item,
+    "12": render_media_builder,
+    "13": update_media_builder,
+    "14": get_media_builder_ids_from_asset
+}
+
+if __name__ == "__main__":
+    print("Which function do you want to run?")
+    for key, value in FUNCTIONS.items():
+        print(f"{key}: {value.__name__}")
+
+    while True:
+        USER_INPUT = input("Enter the number of the function you want to run: ")
+
+        if USER_INPUT in FUNCTIONS:
+            FUNCTIONS[USER_INPUT]()
+            break
+        else:
+            print("Invalid input")
