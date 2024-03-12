3df6b81c0ff62631d7a3b0daac92607d858697ba
scott <slfaludi@gmail.com>
2024-03-12 12:28:19 -0700

    added endpoint

--- asset_upload/Readme.md
+++ asset_upload/Readme.md
@@ -10,14 +10,22 @@
 
 To learn how to download and setup the nomad sdk pip, go to [Nomad SDK PIP](https://github.com/Nomad-Media/nomad-sdk/tree/main/nomad-sdk-pip).
 
-## Upload File
+## Upload Asset
 
-To upload a file, first enter the display name for your asset. If you leave it blank, it will default to the name of your file. Then, enter the upload overwrite option you want to select. Replace replaces an existing asset, continue continues the upload of an asset that is currently uploading, and cancel cancels the upload of an asset that is uploading. Then, enter the file path, related content id you want to add to the asset, and whether or not you want to use multi-thread upload.
+To upload a file, enter 1 when promted. Then enter the display name for your asset. If you leave it blank, it will default to the name of your file. Then, enter the upload overwrite option you want to select. Replace replaces an existing asset, continue continues the upload of an asset that is currently uploading, and cancel cancels the upload of an asset that is uploading. Then, enter the file path, related content id you want to add to the asset, and whether or not you want to use multi-thread upload.
 
 ![](images/upload-asset.png)
 
-![](images/upload-asset-2.png)
-
 > 📘 Note
 > 
-> For more information about the API calls used go to [Asset Upload](ref:asset-upload)
+> For more information about the API calls used go to [Asset Upload](https://developer.nomad-cms.com/docs/start-asset-upload)
+
+## Upload Related Asset
+
+To upload a file as a related asset, enter 2 when promted. Then enter the id of the asset you want to relate the file to in existing asset id. Then, if you want to update the related asset, enter the id of the related asset you want to update. Then, enter the asset metadata type, the file path, and optionally, the id of the language you want to use.
+
+![](images/upload-related-asset.png)
+
+> 📘 Note
+>
+> For more information about the API calls used go to [Related Asset Upload](https://developer.nomad-cms.com/docs/start-related-asset-upload)
--- asset_upload/asset_uploads.py
+++ asset_upload/asset_uploads.py
@@ -7,12 +7,9 @@ import json
 
 def upload_asset():
     try:
-        NAME = input("Enter asset name (press enter if you want name of file to be name): ")
-    
+        NAME = input("Enter asset name: ") if input("Do you want to add an asset name (y/n): ") == "y" else ""
         EXISTING_ASSET_ID = input("Enter existing asset id: ") if input("Do you want to add an existing asset id (y/n): ") == "y" else ""
-        RELATED_ASSET_ID = input("Enter related asset id: ") if input("Do you want to add a related asset id (y/n): ") == "y" else ""
-        LANGUAGE_ID = input("Enter language id: ") if input("Do you want to add a language id (y/n): ") == "y" else ""
-        PARENT_ID = input("Enter parent id: ") if input("Do you want to add a parent id (y/n): ") == "y" else ""
+        RELATED_CONTENT_ID = input("Enter related content id: ") if input("Do you want to add a related content id (y/n): ") == "y" else None
     
         while True:
             UPLOAD_OVERWRITE_OPTION = input("Enter upload overwrite option (replace, continue, cancel): ")
@@ -21,11 +18,11 @@ def upload_asset():
                 break
             print("Invalid input")
         FILE = input("Enter file path: ")
-        RELATED_CONTENT_ID = input("Enter related content id (press enter to skip): ")
+        PARENT_ID = input("Enter parent id: ") if input("Do you want to add a parent id (y/n): ") == "y" else ""
+        LANGUAGE_ID = input("Enter language id: ") if input("Do you want to add a language id (y/n): ") == "y" else ""
     
-        ASSET_INFO = nomad_sdk.upload_asset(NAME, EXISTING_ASSET_ID, RELATED_ASSET_ID,
-                                            RELATED_CONTENT_ID, LANGUAGE_ID, 
-                                            UPLOAD_OVERWRITE_OPTION, FILE, PARENT_ID)
+        ASSET_INFO = nomad_sdk.upload_asset(NAME, EXISTING_ASSET_ID, RELATED_CONTENT_ID,
+                                            UPLOAD_OVERWRITE_OPTION, FILE, PARENT_ID, LANGUAGE_ID)
     
         print(json.dumps(ASSET_INFO, indent=4))
     except Exception as e:
@@ -36,8 +33,6 @@ def upload_related_asset():
         EXISTING_ASSET_ID = input("Enter existing asset id: ")
         RELATED_ASSET_ID = input("Enter related asset id: ") if input("Do you want to add a related asset id (y/n): ") == "y" else None
         NEW_RELATED_ASSET_METADATA_TYPE = input("Enter new related asset metadata type: ")
-        FILE = input("Enter file path: ")
-        LANGUAGE_ID = input("Enter language id: ") if input("Do you want to add a language id (y/n): ") == "y" else None
 
         while True:
             UPLOAD_OVERWRITE_OPTION = input("Enter upload overwrite option (replace, continue, cancel): ")
@@ -45,13 +40,30 @@ def upload_related_asset():
             or UPLOAD_OVERWRITE_OPTION == "cancel":
                 break
             print("Invalid input")
+        FILE = input("Enter file path: ")
+        LANGUAGE_ID = input("Enter language id: ") if input("Do you want to add a language id (y/n): ") == "y" else None
 
-        ASSET_INFO = nomad_sdk.upload_related_asset(EXISTING_ASSET_ID, RELATED_ASSET_ID, NEW_RELATED_ASSET_METADATA_TYPE, UPLOAD_OVERWRITE_OPTION, FILE, LANGUAGE_ID)
+        ASSET_INFO = nomad_sdk.upload_related_asset(EXISTING_ASSET_ID, RELATED_ASSET_ID, NEW_RELATED_ASSET_METADATA_TYPE, FILE, UPLOAD_OVERWRITE_OPTION, LANGUAGE_ID)
 
         print(json.dumps(ASSET_INFO, indent=4))
     except Exception as e:
         print(e)
 
+functions = {
+    "1": upload_asset,
+    "2": upload_related_asset
+}
 
 if __name__ == "__main__":
-    upload_asset()
+    print("Which function do you want to run?")
+    for key, value in functions.items():
+        print(f"{key}: {value.__name__}")
+
+    while True:
+        USER_INPUT = input("Enter the number of the function you want to run: ")
+
+        if USER_INPUT in functions:
+            functions[USER_INPUT]()
+            break
+        else:
+            print("Invalid input")
